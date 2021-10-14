f5b76583a1dc66efe1845ee8a03968795f04c372
Jim Ke <jimke2000@gmail.com>
2021-10-14 14:31:14 +1100

    NASA stuff

---
+++ src/commands/nasa/apod.ts
@@ -0,0 +1,45 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+import request from "request";
+import { Command } from "../../interfaces/Command";
+
+export = class APOD extends Command {
+  name = "apod";
+  disabled? = false;
+  description = "Fetch the Astronomy Pictrue of the Day from NASA";
+  usage = "";
+  aliases = ["npod", "astronomypictureoftheday"];
+  args = false;
+  example = "";
+  cooldown = 120_000; // 2 mins
+  category = "nasa";
+  guildOnly = false;
+  data = new SlashCommandBuilder()
+    .setName(this.name)
+    .setDescription(this.description);
+  sudo = false;
+
+  execute = async (interaction: CommandInteraction) => {
+    await interaction.deferReply();
+    request(
+      "https://api.nasa.gov/planetary/apod?api_key=" + process.env.NASA_API,
+      {
+        json: true,
+      },
+      (err, res, body) => {
+        if (err)
+          return interaction.editReply({ embeds: [this.errorEmbed(err)] });
+        const embed = this.embed(
+          {
+            author: { name: body.copyright ?? "NASA" },
+            title: "NASA's Astronomy Picture of the Day",
+            image: { url: body.hdurl },
+            description: body.explanation,
+          },
+          interaction
+        );
+        return interaction.editReply({ embeds: [embed] });
+      }
+    );
+  };
+};
---
+++ src/commands/nasa/mars.ts
@@ -0,0 +1,55 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+import request from "request";
+import { Command } from "../../interfaces/Command";
+
+export = class Mars extends Command {
+  name = "mars";
+  disabled? = false;
+  description = "Fetch some photos taken on Mars by NASA's astronomy rovers";
+  usage = "";
+  aliases = ["marsrover", "marsimg"];
+  args = false;
+  example = "";
+  cooldown = 120_000; // 2 mins
+  category = "nasa";
+  guildOnly = false;
+  data = new SlashCommandBuilder()
+    .setName(this.name)
+    .setDescription(this.description);
+  sudo = false;
+
+  execute = async (interaction: CommandInteraction) => {
+    await interaction.deferReply();
+    request(
+      `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${this.rnd(
+        1,
+        1000
+      )}&api_key=${process.env.NASA_API}`,
+      {
+        json: true,
+      },
+      (err, res, body) => {
+        if (err)
+          return interaction.editReply({ embeds: [this.errorEmbed(err)] });
+        const { photos } = body;
+        const photo = photos[Math.floor(Math.random() * photos.length)];
+        const embed = this.embed(
+          {
+            author: { name: "Curiosity Mars Rover" },
+            image: { url: photo.img_src },
+            fields: [
+              {
+                name: "Metadata",
+                value: `**Taken on Mars day:** ${photo.sol}\n**Photo ID:** ${photo.id}`,
+                inline: true,
+              },
+            ],
+          },
+          interaction
+        );
+        interaction.editReply({ embeds: [embed] });
+      }
+    );
+  };
+};
