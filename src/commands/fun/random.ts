3ffe1fae3aa41dfacf637bd72da67c6862421446
Jim Ke <jimke2000@gmail.com>
2021-10-14 13:31:28 +1100

    Added random command

---
+++ src/commands/fun/random.ts
@@ -0,0 +1,75 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction, MessageAttachment } from "discord.js";
+import { Command } from "../../interfaces/Command";
+import { Fun, oneToSix } from "../../static/methods/Fun";
+
+export = class Random extends Command {
+  name = "random";
+  disabled? = false;
+  description = "Generate a random number";
+  usage = "<min> <max>";
+  aliases = ["rnd", "rand"];
+  args = true;
+  example = "1 10";
+  cooldown = 1_000;
+  category = "fun";
+  guildOnly = false;
+  data = new SlashCommandBuilder()
+    .setName(this.name)
+    .setDescription(this.description)
+    .addNumberOption((opt) =>
+      opt
+        .setName("min")
+        .setDescription("Minimum number to generate (included)")
+        .setRequired(true)
+    )
+    .addNumberOption((opt) =>
+      opt
+        .setName("max")
+        .setDescription("Maximum number to generate (included)")
+        .setRequired(true)
+    );
+  sudo = false;
+
+  execute = async (interaction: CommandInteraction) => {
+    const crazy = 9007199254740991;
+    const min = interaction.options.getNumber("min");
+    const max = interaction.options.getNumber("max");
+    if (min === null || max === null)
+      return interaction.reply({
+        embeds: [this.errorEmbed("You must fill in all the fields!")],
+      });
+    if (min <= 0 || max <= 0)
+      return interaction.reply({
+        embeds: [this.errorEmbed("Number must be above zero")],
+      });
+    if (min === max)
+      return interaction.reply({
+        embeds: [this.errorEmbed("Number must not equal each other")],
+      });
+    if (min > max)
+      return interaction.reply({
+        embeds: [this.errorEmbed("Maximum must be above minimum")],
+      });
+    if (min > crazy || max > crazy)
+      return interaction.reply({
+        embeds: [
+          this.errorEmbed(
+            "Maximum integer size execeeded! 9007199254740991 is the largest number permitted!"
+          ),
+        ],
+      });
+    const num = this.rnd(min as number, max as number);
+    interaction.reply({
+      embeds: [
+        this.embed(
+          {
+            title: "Your random number is...",
+            description: num.toString(),
+          },
+          interaction
+        ),
+      ],
+    });
+  };
+};
