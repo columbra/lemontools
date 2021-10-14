06ee18b959bd7c4f8c30a73637c8ca8705034ee1
Jim Ke <jimke2000@gmail.com>
2021-10-14 14:53:21 +1100

    doggos

---
+++ src/commands/fun/dog.ts
@@ -0,0 +1,41 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction } from "discord.js";
+import request from "request";
+import { Command } from "../../interfaces/Command";
+
+export = class Dog extends Command {
+  name = "dog";
+  disabled? = false;
+  description = "Aww";
+  usage = "";
+  aliases = ["doggo", "doggy"];
+  args = false;
+  example = "";
+  cooldown = 5_000;
+  category = "fun";
+  guildOnly = false;
+  data = new SlashCommandBuilder()
+    .setName(this.name)
+    .setDescription(this.description);
+  sudo = false;
+
+  execute = async (interaction: CommandInteraction) => {
+    interaction.deferReply();
+    request(
+      "https://dog.ceo/api/breeds/image/random",
+      { json: true },
+      (err, res, body) => {
+        const embed = this.embed(
+          {
+            title: "Random doggo",
+            image: {
+              url: body.message,
+            },
+          },
+          interaction
+        );
+        interaction.editReply({ embeds: [embed] });
+      }
+    );
+  };
+};
