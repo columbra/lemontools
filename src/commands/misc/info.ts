9f675a9786023feaa498564b2ede24769acf61bd
Jim Ke <jimke2000@gmail.com>
2021-10-14 16:04:39 +1100

    Added description

--- src/commands/misc/info.ts
+++ src/commands/misc/info.ts
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, Permissions } from "discord.js";
 import { Command } from "../../interfaces/Command";
 import * as config from "../../../config.json";
 
@@ -27,10 +27,17 @@ export = class Info extends Command {
           config.author
         }\n**Language:** TypeScript\n**Production Ready:** ${
           config.production ? "Yes" : "No"
-        }`,
+        }\n\n**About:**\n${
+          config.name
+        } is a multi-use tools bot, which has a multitude of features not covered by other bots, such as QR Code creation, conversation starters and much, much more. Invite me to your servers today! Invite: **[Link](${this.bot.generateInvite(
+          {
+            scopes: ["bot", "applications.commands"],
+            permissions: new Permissions(448928796608n),
+          }
+        )})**`,
       },
       interaction
     );
     interaction.reply({ embeds: [embed] });
   };
-}
+};
