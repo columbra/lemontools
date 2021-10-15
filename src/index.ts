795e2e521492a6cf4481b387a127b90ec14f6985
Jim Ke <jimke2000@gmail.com>
2021-10-15 11:12:22 +1100

    Added @see

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,10 @@
 
         You should have received a copy of the GNU General Public License
         along with this program.  If not, see <https://www.gnu.org/licenses/>.
- * 
+ *
+ */
+/**
+ * @see https://cooljim.github.io/lemontools/
  */
 import * as File from "../config.json"
 import { Bot } from "./client/Client"
