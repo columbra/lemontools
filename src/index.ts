8a085b19fb23f95bd257e51bd075e3515d04bce4
Jim Ke <jimke2000@gmail.com>
2021-12-26 21:42:08 +1100

    feat(*): ASCII art

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,38 @@
  * See LICENSE for license information
  * 
  * :)
+ *                                                                                                                                 -::+hhhhhhhhhhhhs- 
+           `ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssshhhyssss:.....+yhhs
+           .hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhyy/::-           .+hh
+          `/hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh+yhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs+/``                :hh
+          :hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhy yhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs:`                   :shh
+          :hhhhho//////////////////shhhh////////////////////yhhy///shhhhhhhhhy yhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs/:`   ````            `:shy/
+         /yhhho-`.ssssssssssssss   +hhhh.  -ssssssssssss+  -yhhs   ohhhhhhhhs` yhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs:`  ---+yyss/`          shhy+ 
+         /hhhh.  -hhhhhhhhhhhhs-  /yhhh-  -shhhhhhhhhhho.  hhhy+  +yhhhhhhhhs  oyhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs:..:oohhhhhhhhh/        .ohh+.  
+        :shhhh.  -hhhhhhhhhhhho   ohhhh   /hhhhhhhhhhhh/   yhh+`  yhhhhhhhhhs  `ohhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhy++/syyhhhhhhhhhy+.    ``-+yhy+.   
+        ohhhho` :yhhhhhhhhhhhhs---shhhh  .+hhhhhhhhhhhh/  -hhh+   yhhhhhhhhhs   ohhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhshhhhhhhhhhhhhh-`    -osyhho.`    
+        ohhhh   /hhhhhhhhhhhhhhhhhhhhy-  ohhhhhhhhhhhh+. `hhhy/  oyhhhhhhhhhs   +yhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs-    `+yhhhhy`      
+       :yhhhy  -ohhhhhhhhhhhhhhhhhhhhs   ohhhhhhhhhhhh-  .hhh/   hhhhhhhhhhhs    /hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs:`      ``//ohhy`     
+       yhhhy.  +hhhhhhhhhhhhhhhhhhhhy+  .ohhhhhhhhhhhs- .shhh: `-hhhhhhhhhhhs    /hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhss:`            `:hh`     
+       yhhhs   +hhhhhho::::::::/hhhh+   shhhhhhhhhhhh`  -hhh/. .hhhhhhhhhhhhs    /hhhhhhhhhhh+ohhhhhhhhhhhhhhhhhhhhhhhhhhhy:                -ohh`     
+     `hhhhhs  -shhhhhhhhhhhh-  `hhhh+   ::::::::::::::syhhhh.  .hhhhhhhhhhhhs     -hhhhhhhh+/shhhhhhhhhhhhhhhhhhhhhhhhho+:`::::::::         /hhh`     
+     `hhhho.  shhhhhhhhhhhh-` .shhhy/  shhhhhhhhhhhhhhhhhhhh. `:hhhhhhhhhhhhs     -shhhhhs..yhhhhhhhhhhhhhhhhhhhhhhhhh/ossshhhhhhhh`      .+yhy.      
+    .+hhhh+   shhhhhhhhhhhh.  -hhhh:   hhhhhhhhhhhhhhhhhhhh:` :hhhhhhhhhhhhhs      .hhhho:+hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhy:       shhs:       
+    -hhhhs:  yhhhhhhhhhhhhh.  -hhhh:   hhhhhhhhhhhhhhhhhhhh`  :hhhhhhhhhhhhhs     `/hho-./hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs/      `:yhh+         
+    -hhhh/   hhhhhhhhhhhhh.  :shhhs- .shhhhhhhhhhhhhhhhhhhs  :yhhhhhhhhhhhhhs.   :shhy. :hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs:`     `:shhs:`         
+   -ohhhh/   yyyyyyyyyyyyy   /hhhh-  .hhhhhhhhhhhhhhhhhhhy`  /yyyyyyyyyyyhhhhy  :shy/` -ohhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhyy/:`     `:shhy/`           
+   /hhhhh+-- `````````````--oyhhhh- `/hhhhhhhhhhhhhhhhhhhy   ````````````shhhy  ohy+   +hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs:``       -shhy+`             
+  `+hhhhhhhhoooooooooooooohhhhhhhhsoshhhhhhhhhhhhhhhhhhhhhoooooooooooooooyhhhh.oyho    /yhhhhhhhhhhhhhhhhhhhhhhhhhyss--`      `:oohhy/.               
+  +hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhyhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhyhho-     :yhhhhhhhhhhhhhhhhhhhyyy+/-``      ``:shhy//.                 
+  +hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhoohhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh/      `-oohhhhhhhhhhhhoooo/...         `:sshhy+.                    
+ +yhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh+ohhhhhhhhhhhhhhhhhhhhhhdhhhhhhhhhhhhhhh/          ------------              `/oshhy:-.                      
+ shhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhdhhhhhy+hhhhhhhhhhhhhhhhhhhhhhhhhhhhh/                                `.//yhhs//`                         
+ shhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhshhhhhhhhhhhhhhhhhhhhhhhhhhhhh/                             --+sshhso/`                            
++yhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs/`                     ```-++hhyys::                                
+hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhy:                .:::hhyhhh++.                                    
+hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhsss/.........sssyhhhsss+..                                       
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::hhhhhhhhhhhhhhh+::::                                             
+ *
  */
 import * as dotenv from "./lib/dotenv";
 import path from "path";
