44f727031ae1a47e0770773e0c6081894b090d3c
Jim Ke <jimke2000@gmail.com>
2021-12-30 19:09:31 +1100

    fix(util): get rid of .body

--- src/util/web.ts
+++ src/util/web.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export async function getJSON<T = unknown>(url: string) {
-  const res: T = JSON.parse((await axios.get(url)).data);
+  const res: T = JSON.parse((await axios.get(url)));
   return res;
 }
