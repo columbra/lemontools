c3eb395e88dc839ab1e8e81eee3f374a91206f66
Jim Ke <jimke2000@gmail.com>
2021-12-30 19:34:25 +1100

    feat(util): new emojis

--- src/util/emoji.ts
+++ src/util/emoji.ts
@@ -2,4 +2,6 @@ export const enum LemonEmojis {
   Node = "<:lemonnodejs:922641121417515008>",
   LemonTools = "<:lemontools:914003790636675122>",
   v8 = "<:lemonv8:924575256436432937>",
+  Karma = "<:lemonkarma:926020501170692126>",
+  Gilds = "<:lemonawards:926022127860195368>",
 }
