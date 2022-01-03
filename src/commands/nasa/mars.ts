2f381d12f1d115b891fde3b132485f0e8a592e18
Jim Ke <jimke2000@gmail.com>
2022-01-03 11:52:51 +1100

    feat(command): /mars for mars imgs

---
+++ src/commands/nasa/mars.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+import Command from "../../classes/Command";
+import { MarsPhoto } from "../../typings/nasa/mars";
+import { embed } from "../../util/embed";
+import { rnd } from "../../util/number";
+
+export default new Command({
+  name: "mars",
+  description: "Get some pictures of Mars from the rover Curiosity",
+  category: "nasa",
+  perms: [],
+
+  async execute({ bot, ctx }) {
+    await ctx.deferReply();
+    const res = await axios.get(
+      `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${rnd(
+        // Controls what mars day
+        1, // the photos come from (1 day on mars = 1 sol). This is randomised, so we should get a random
+        1000 // image each time (hopefully)
+      )}&api_key=${process.env.NASA}`
+    );
+    const { photos } = res.data;
+    let photo: MarsPhoto = photos[Math.floor(Math.random() * photos.length)];
+    while (!photo) photo = photos[Math.floor(Math.random() * photos.length)];
+    const em = embed(
+      {
+        image: {
+          url: photo.img_src,
+        },
+        description: `Photo ID: \`${photo.id}\`
+      Mars Day: \`${photo.sol}\``,
+      },
+      ctx,
+      bot
+    );
+    ctx.editReply({
+      embeds: [em],
+    });
+  },
+});
