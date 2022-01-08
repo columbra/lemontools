3a7b76b5687e4490da64e2ec658e7e4a4fd1be3e
Jim Ke <jimke2000@gmail.com>
2022-01-08 18:43:48 +1100

    feat(util): template function to generate errors

--- src/util/strings.ts
+++ src/util/strings.ts
@@ -1,3 +1,16 @@
+import { InteractionReplyOptions, MessageEmbed } from "discord.js";
+
 export function capitalise(str: string) {
   return str[0].toUpperCase() + str.substring(1);
+}
+export function epherr(strings: TemplateStringsArray): InteractionReplyOptions {
+  return {
+    embeds: [
+      new MessageEmbed({
+        description: strings.raw[0],
+        color: "RED",
+      }),
+    ],
+    ephemeral: true,
+  };
 }
