65112531f140bbba70ffcbc2d01578a8413ed402
Jim <jimke2000@gmail.com>
2022-01-26 12:35:09 +1100

    feat(schema): remove msg option in welcome schema
    Signed-off-by: Jim <jimke2000@gmail.com>

--- src/schema/Welcome.ts
+++ src/schema/Welcome.ts
@@ -2,14 +2,12 @@ import { MessageEmbedOptions } from "discord.js";
 import { model, Schema } from "mongoose";
 
 const WelcomeSchema = new Schema({
-  message: String,
   embed: Object,
   serverId: String,
   lastChangesBy: String,
 });
 
 export interface WelcomeInterface {
-  message: string;
   embed: MessageEmbedOptions;
   serverId: string;
   lastChangesBy: string;
