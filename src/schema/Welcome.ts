3f3d4b5012c92a1746726188d9ab4a6f65563996
Jim <jimke2000@gmail.com>
2022-01-26 12:29:48 +1100

    feat(schema): add types for schema
    Signed-off-by: Jim <jimke2000@gmail.com>

--- src/schema/Welcome.ts
+++ src/schema/Welcome.ts
@@ -1,12 +1,18 @@
+import { MessageEmbedOptions } from "discord.js";
 import { model, Schema } from "mongoose";
 
-const WelcomeSchema = new Schema (
-  {
-    message: String,
-    embed: Object,
-    serverId: String,
-    lastChangesBy: String
-  }
-)
+const WelcomeSchema = new Schema({
+  message: String,
+  embed: Object,
+  serverId: String,
+  lastChangesBy: String,
+});
 
-export default model("WelcomeScheme", WelcomeSchema)
+export interface WelcomeInterface {
+  message: string;
+  embed: MessageEmbedOptions;
+  serverId: string;
+  lastChangesBy: string;
+}
+
+export default model("WelcomeScheme", WelcomeSchema);
