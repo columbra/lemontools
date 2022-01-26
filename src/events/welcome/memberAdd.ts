a46ef237a9467fb513150ab10ca5986ddcbe4564
Jim <jimke2000@gmail.com>
2022-01-26 12:36:36 +1100

    feat(event): add listener to send welcomedm
    Signed-off-by: Jim <jimke2000@gmail.com>

---
+++ src/events/welcome/memberAdd.ts
@@ -0,0 +1,19 @@
+import Event from "../../classes/Event";
+import Welcome, { WelcomeInterface } from "../../schema/Welcome";
+
+export default new Event("guildMemberAdd", async (bot, member) => {
+  const welcome: WelcomeInterface | undefined = await Welcome.findOne({
+    serverId: member.guild.id,
+  }).exec();
+  if (!welcome) return; // No welcome found === stop
+  member
+    .send({
+      content: `This message was sent from the server **${member.guild.name}**`,
+      embeds: [welcome.embed],
+    })
+    .catch((r) =>
+      bot.logger.warn(
+        `Something went wrong DMing user ${member.user.tag} a welcome DM from server ${member.guild.name}. They may have blocked us! Reason: ${r}`
+      )
+    );
+});
