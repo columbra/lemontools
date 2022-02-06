d7fe6cd9d1031c1feddaee06bacce4d339f6b8e8
Jim <jimke2000@gmail.com>
2022-02-06 20:54:36 +1100

    feat(command): remove long footer
    Signed-off-by: Jim <jimke2000@gmail.com>

--- src/commands/server/welcome.ts
+++ src/commands/server/welcome.ts
@@ -71,7 +71,7 @@ export default new Command({
           title,
           description,
           footer: {
-            text: `Powered by ${bot.config.bot.name}. This bot is providing a service, to disable further DMs, please use the /block command to configure blocking.`,
+            text: `Powered by ${bot.config.bot.name}.`,
             iconURL: ctx.guild.iconURL(),
           },
         };
