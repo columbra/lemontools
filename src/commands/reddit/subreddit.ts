dd7faa0c9a0d1f8807f20e8663d93e7a3fd65bf8
Jim <jimke2000@gmail.com>
2022-02-15 21:18:57 +1100

    fix(command): non nsfw posts not appearing
    Signed-off-by: Jim <jimke2000@gmail.com>

--- src/commands/reddit/subreddit.ts
+++ src/commands/reddit/subreddit.ts
@@ -53,9 +53,9 @@ export default new Command({
         )
       );
     const { children: posts } = data;
-    const { data: post } = posts.filter((p) => p.data.over_18 && isNSFW)[
-      rnd(0, posts.length)
-    ] || { data: null }; // Remove over_18 (nsfw) posts then randomly select a post
+    const { data: post } = posts.filter((p) =>
+      p.data.over_18 ? isNSFW : true
+    )[rnd(0, posts.length)] || { data: null }; // Remove over_18 (nsfw) posts then randomly select a post
     if (!post)
       return ctx.editReply(
         errorMessage(
