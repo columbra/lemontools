569adc320011a62ad7f6ce4badca9aac66cf975f
Jim <jimke2000@gmail.com>
2022-02-20 11:33:20 +1100

    fix(events): modify how delete interaction works
    Signed-off-by: Jim <jimke2000@gmail.com>

--- src/events/interactions/deleteInteraction.ts
+++ src/events/interactions/deleteInteraction.ts
@@ -2,7 +2,7 @@ import Event from "../../classes/Event";
 
 export default new Event("interactionCreate", async (bot, ctx) => {
   if (!ctx.isButton()) return;
-  if (ctx.customId !== "delete") return;
+  if (!ctx.customId.includes("delete_")) return;
   ctx.deleteReply().catch((err) => {
     bot.logger.warn(`Error deleting interaction ${ctx.id}, error ${err}`);
   });
