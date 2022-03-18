a23106a9606e9e31f0d7e093ccc67a01a21acf62
Jim <jimke2000@gmail.com>
2022-03-18 13:16:47 +1100

    feat(*): welcome
    Signed-off-by: Jim <jimke2000@gmail.com>

--- src/classes/Bot.ts
+++ src/classes/Bot.ts
@@ -82,7 +82,7 @@ export default class Bot extends Client {
   public cache = new CacheManager({});
   constructor() {
     super({
-      intents: ["GUILDS", "GUILD_MESSAGE_REACTIONS" /*, "GUILD_MEMBERS"*/],
+      intents: ["GUILDS", "GUILD_MESSAGE_REACTIONS", "GUILD_MEMBERS"],
       // Credit: salvage
       /**
        * @author Salvage_Dev#3650
@@ -271,7 +271,7 @@ export default class Bot extends Client {
       this.application?.commands.set(commands);
     }
   }
-   isDev() {
+  isDev() {
     if (
       process.env.ENVIRONMENT === "debug" ||
       process.env.ENVIRONMENT === "dev"
