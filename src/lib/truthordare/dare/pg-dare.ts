1d15d226bcc909080be6d6e77eff6b18a2548986
Jim <jimke2000@gmail.com>
2022-03-28 19:26:03 +1100

    feat(lib): more dares + truths
    Signed-off-by: Jim <jimke2000@gmail.com>

--- src/lib/truthordare/dare/pg-dare.ts
+++ src/lib/truthordare/dare/pg-dare.ts
@@ -1,3 +1,14 @@
 export default [
   "Everyone go and drink a sip of water. -Compositr, Lemon Tools Dev",
+  "Send a random DM to the first person in your friends list",
+  "Let the group decide what PFP you have for the next day",
+  "Let the group decide what PFP you have for the next week",
+  "Let the group nickname you in this server to whatever they want",
+  "Do your best impression of someone else in the group",
+  "Link everyone to the last song you played",
+  "Link everyone to your favourite playlist",
+  "Show everyone the first 10 people on your friends list",
+  "Show everyone the first 10 people on your block list",
+  "Write a poem using the words orange and moose at least once each",
+  "Text someone in the group, and the group has to guess who you texted",
 ] as const;
--- src/lib/truthordare/truth/pg-truth.ts
+++ src/lib/truthordare/truth/pg-truth.ts
@@ -1,1 +1,19 @@
-export default ["What was the most embarassing moment of your life?"] as const;
+export default [
+  "What was the most embarassing moment of your life?",
+  "Who is the ugliest out of everyone playing in your opinion?",
+  "How often do you bathe?",
+  "When was the last time you cried?",
+  "What is the most embarassing thing you have done?",
+  "What is your favourite sport",
+  "What is your favourite food?",
+  "What is your favourite drink?",
+  "What is your favourite colour?",
+  "What is your favourite animal?",
+  "If you had a billion dollars all of a sudden, how would you spend it",
+  "What is your favourite song?",
+  "What is something you haven't said to anyone else? Say that now",
+  "Tell the group what your last dream was about",
+  "What is your favourite thing to do in your free time?",
+  "State the dumbest thing you have ever done",
+  "If you could become a fictional character for one day, who would it be?",
+] as const;
