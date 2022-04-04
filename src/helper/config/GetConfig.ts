d92549c3a0c47f9ac2ca97688879c469e582cd6e
Jim <jimke2000@gmail.com>
2022-04-04 20:44:52 +1000

    feat(helper): get config
    Signed-off-by: Jim <jimke2000@gmail.com>

---
+++ src/helper/config/GetConfig.ts
@@ -0,0 +1,7 @@
+import { readFileSync } from "fs";
+import { load } from "js-yaml";
+import path from "path";
+
+export default function getConfig(): Readonly<Record<string, any>> {
+  return load(readFileSync(path.join(__dirname, "../../config.yaml"), "utf-8"));
+}
