9fd01324b6898749774220ba2a882a5fe91c3da2
Jim <jimke2000@gmail.com>
2022-04-05 19:52:53 +1000

    feat(schema): purge
    Signed-off-by: Jim <jimke2000@gmail.com>

--- src/schema/UserBlockingPreferences.ts
+++
@@ -1,8 +0,0 @@
-import { model, Schema } from "mongoose";
-
-const UserBlockingPreferencesSchema = new Schema({
-  userId: String,
-  blocks: [String],
-})
-
-export default model("UserBlockingPreferences", UserBlockingPreferencesSchema);
