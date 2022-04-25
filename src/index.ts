169053b523c14334502c591c8d66fa1483f62b42
Jim (Compositr) <jimke2000@gmail.com>
2022-04-25 17:37:31 +1000

    refactor: clean up stuff
    Signed-off-by: Jim (Compositr) <jimke2000@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,44 +2,11 @@
  * @copyright © Copyright Lemon Bots 2022
  * @license GPL-3.0-only
  * @author Jim Ke
- * 
+ *
  * Lemon Tools v3 Source Code
  * See LICENSE for license information
- * 
- * :)
- *                                                                                                                                 -::+hhhhhhhhhhhhs- 
-           `ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssshhhyssss:.....+yhhs
-           .hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhyy/::-           .+hh
-          `/hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh+yhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs+/``                :hh
-          :hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhy yhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs:`                   :shh
-          :hhhhho                  shhhh                    yhhy   shhhhhhhhhy yhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs/:`   ````            `:shy/
-         /yhhho-`.ssssssssssssss   +hhhh.  -ssssssssssss+  -yhhs   ohhhhhhhhs` yhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs:`  ---+yyss/`          shhy+ 
-         /hhhh.  -hhhhhhhhhhhhs-  /yhhh-  -shhhhhhhhhhho.  hhhy+  +yhhhhhhhhs  oyhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs:..:oohhhhhhhhh/        .ohh+.  
-        :shhhh.  -hhhhhhhhhhhho   ohhhh   /hhhhhhhhhhhh/   yhh+`  yhhhhhhhhhs  `ohhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhy++/syyhhhhhhhhhy+.    ``-+yhy+.   
-        ohhhho` :yhhhhhhhhhhhhs---shhhh  .+hhhhhhhhhhhh/  -hhh+   yhhhhhhhhhs   ohhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhshhhhhhhhhhhhhh-`    -osyhho.`    
-        ohhhh   /hhhhhhhhhhhhhhhhhhhhy-  ohhhhhhhhhhhh+. `hhhy/  oyhhhhhhhhhs   +yhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs-    `+yhhhhy`      
-       :yhhhy  -ohhhhhhhhhhhhhhhhhhhhs   ohhhhhhhhhhhh-  .hhh/   hhhhhhhhhhhs    /hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs:`      ``//ohhy`     
-       yhhhy.  +hhhhhhhhhhhhhhhhhhhhy+  .ohhhhhhhhhhhs- .shhh: `-hhhhhhhhhhhs    /hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhss:`            `:hh`     
-       yhhhs   +hhhhhho::::::::/hhhh+   shhhhhhhhhhhh`  -hhh/. .hhhhhhhhhhhhs    /hhhhhhhhhhh+ohhhhhhhhhhhhhhhhhhhhhhhhhhhy:                -ohh`     
-     `hhhhhs  -shhhhhhhhhhhh-  `hhhh+   ::::::::::::::syhhhh.  .hhhhhhhhhhhhs     -hhhhhhhh+/shhhhhhhhhhhhhhhhhhhhhhhhho+:`::::::::         /hhh`     
-     `hhhho.  shhhhhhhhhhhh-` .shhhy/  shhhhhhhhhhhhhhhhhhhh. `:hhhhhhhhhhhhs     -shhhhhs..yhhhhhhhhhhhhhhhhhhhhhhhhh/ossshhhhhhhh`      .+yhy.      
-    .+hhhh+   shhhhhhhhhhhh.  -hhhh:   hhhhhhhhhhhhhhhhhhhh:` :hhhhhhhhhhhhhs      .hhhho:+hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhy:       shhs:       
-    -hhhhs:  yhhhhhhhhhhhhh.  -hhhh:   hhhhhhhhhhhhhhhhhhhh`  :hhhhhhhhhhhhhs     `/hho-./hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs/      `:yhh+         
-    -hhhh/   hhhhhhhhhhhhh.  :shhhs- .shhhhhhhhhhhhhhhhhhhs  :yhhhhhhhhhhhhhs.   :shhy. :hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs:`     `:shhs:`         
-   -ohhhh/   yyyyyyyyyyyyy   /hhhh-  .hhhhhhhhhhhhhhhhhhhy`  /yyyyyyyyyyyhhhhy  :shy/` -ohhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhyy/:`     `:shhy/`           
-   /hhhhh+-- `````````````--oyhhhh- `/hhhhhhhhhhhhhhhhhhhy   ````````````shhhy  ohy+   +hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs:``       -shhy+`             
-  `+hhhhhhhhoooooooooooooohhhhhhhhsoshhhhhhhhhhhhhhhhhhhhhoooooooooooooooyhhhh.oyho    /yhhhhhhhhhhhhhhhhhhhhhhhhhyss--`      `:oohhy/.               
-  +hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhyhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhyhho-     :yhhhhhhhhhhhhhhhhhhhyyy+/-``      ``:shhy//.                 
-  +hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhoohhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh/      `-oohhhhhhhhhhhhoooo/...         `:sshhy+.                    
- +yhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh+ohhhhhhhhhhhhhhhhhhhhhhdhhhhhhhhhhhhhhh/          ------------              `/oshhy:-.                      
- shhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhdhhhhhy+hhhhhhhhhhhhhhhhhhhhhhhhhhhhh/                                `.//yhhs//`                         
- shhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhshhhhhhhhhhhhhhhhhhhhhhhhhhhhh/                             --+sshhso/`                            
-+yhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhs/`                     ```-++hhyys::                                
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhy:                .:::hhyhhh++.                                    
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhsss/.........sssyhhhsss+..                                       
-:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::hhhhhhhhhhhhhhh+::::                                             
- *
  */
+
 import * as dotenv from "./lib/dotenv";
 import path from "path";
 import Bot from "./classes/Bot";
@@ -47,6 +14,7 @@ import Bot from "./classes/Bot";
 const { parsed } = dotenv.config({
   path: path.join(__dirname, `../.env`),
 });
+
 for (const key in parsed) {
   if (Object.prototype.hasOwnProperty.call(parsed, key)) {
     const element = parsed[key];
