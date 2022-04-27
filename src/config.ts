92c4fce0b1988dc112c62a4e3a6427d6930b7d15
Jim (Compositr) <jimke2000@gmail.com>
2022-04-27 21:11:34 +1000

    feat(config): add intents to config
    Signed-off-by: Jim (Compositr) <jimke2000@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -3,14 +3,23 @@
  * @since v3.0.0
  */
 
+import type { BitFieldResolvable, ClientOptions, IntentsString } from "discord.js";
+
 export default {
   style: {
     colours: {
-      primary: "#FEF250"
+      primary: "#FEF250",
     },
     name: {
       full: "Lemon Tools",
-      short: "Lemon Tools"
-    }
-  }
-} as const
+      short: "Lemon Tools",
+    },
+  },
+  bot: {
+    // Intents will take precedence over options specified below
+    intents: ["GUILDS", "GUILD_MEMBERS"] as BitFieldResolvable<IntentsString, number>,
+    options: {
+
+    } as ClientOptions
+  },
+} as const;
