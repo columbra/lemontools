bf113b21cabcc711761c6eb976ebbf1fb6856bc5
Jim (Compositr) <jimke2000@gmail.com>
2022-04-27 19:46:20 +1000

    feat(classes): extend message embed
    Signed-off-by: Jim (Compositr) <jimke2000@gmail.com>

---
+++ src/classes/embeds/LemonToolsEmbed.ts
@@ -0,0 +1,23 @@
+/**
+ * @fileoverview Custom constructor for a MessageEmbed with utility stuff
+ * @since v3.0.0
+ */
+
+import { MessageEmbed } from "discord.js";
+import type { User, MessageEmbedOptions } from "discord.js";
+import config from "../../config";
+
+export default class LemonToolsEmbed extends MessageEmbed {
+  constructor(data: MessageEmbedOptions = {}, user?: User) {
+    super(data);
+    this.setColor(config.style.colours.primary);
+    if (user)
+      return this.setFooter({
+        text: `${user.tag} | Powered by ${config.style.name.short}`,
+        iconURL: user.displayAvatarURL(),
+      });
+    this.setFooter({
+      text: `Powered by ${config.style.name.short}`,
+    });
+  }
+}
