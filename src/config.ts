6c2ff9c5014037c262656d6fce3ed80cdee63dec
Jim (Compositr) <jimke2000@gmail.com>
2022-04-27 19:08:44 +1000

    feat(v3): new configuration for better typings
    Signed-off-by: Jim (Compositr) <jimke2000@gmail.com>

---
+++ src/config.ts
@@ -0,0 +1,16 @@
+/**
+ * @fileoverview Configuration file for LemonTools. Basically a collection of non-secret constants
+ * @since v3.0.0
+ */
+
+export default {
+  style: {
+    colours: {
+      primary: "#FEF250"
+    },
+    name: {
+      full: "Lemon Tools",
+      short: "Lemon Tools"
+    }
+  }
+} as const
