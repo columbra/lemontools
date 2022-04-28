e3fbfbe6bfe0a7acbc1c5ff93a0d24a38b7647a6
Jim (Compositr) <jimke2000@gmail.com>
2022-04-28 22:22:45 +1000

    feat(v3): custom client
    Signed-off-by: Jim (Compositr) <jimke2000@gmail.com>

---
+++ src/LemonTools.ts
@@ -0,0 +1,25 @@
+/**
+ * @fileoverview Extended version of the Discord.js Client
+ * @since v3.0.0
+ */
+
+import { Client, Options } from "discord.js";
+import config from "./config";
+import Logger from "./managers/logs/Logger";
+
+export default class LemonTools extends Client {
+  // Managers
+  public Logger = new Logger(this);
+
+  constructor() {
+    super({
+      ...config.bot.options,
+      intents: config.bot.intents,
+      makeCache: Options.cacheWithLimits(config.bot.cache),
+    });
+  }
+
+  async start() {
+    await this.login(process.env.BOT_TOKEN);
+  }
+}
