beb2510273f4654f20ea931810fb7707580705f6
Jim (Compositr) <jimke2000@gmail.com>
2022-05-03 17:25:11 +1000

    feat(managers): register events
    Signed-off-by: Jim (Compositr) <jimke2000@gmail.com>

--- src/managers/events/Events.ts
+++ src/managers/events/Events.ts
@@ -5,9 +5,37 @@
 
 import Manager from "../../classes/manager/Manager";
 import type LemonTools from "../../LemonTools";
+import FileSystemUtils from "../../utils/files/FileSystemUtils";
+import type EventListener from "../../classes/events/EventListener";
+import type { ClientEvents } from "discord.js";
 
 export default class Events extends Manager {
   constructor(lemontools: LemonTools) {
     super(lemontools, "Events");
+    this.load();
+  }
+
+  async load(path = "event/**/*") {
+    const events = await FileSystemUtils.importGlob<
+      EventListener<keyof ClientEvents>
+    >(path);
+    await Promise.all(
+      events.map(async (event) => {
+        this.lemontools.on(event.event, (...args: any) => {
+          event.execute(
+            {
+              event: event.event,
+              lemontools: this.lemontools,
+            },
+            ...args
+          );
+        });
+      })
+    );
+    this.lemontools.Logger.log(
+      "info",
+      "Events",
+      `Loaded ${events.length} events`
+    );
   }
 }
