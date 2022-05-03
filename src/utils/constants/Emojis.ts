3235cfe4056da484819da0808156cd064c42fd07
Jim (Compositr) <jimke2000@gmail.com>
2022-05-03 18:56:31 +1000

    feat(utils): emoji enum
    Signed-off-by: Jim (Compositr) <jimke2000@gmail.com>

---
+++ src/utils/constants/Emojis.ts
@@ -0,0 +1,8 @@
+enum Emojis {
+  IDLE = "<:lemonidle:929328418577870948>",
+  OFFLINE = "<:lemonoffline:929323412730552370>",
+  ONLINE = "<:lemononline:929321893759168543>",
+  LEMONTOOLS = "<:lemontools:914003790636675122>",
+}
+
+export default Emojis;
