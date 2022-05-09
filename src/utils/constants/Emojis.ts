13a78a3b6f3b911ea3b3a9d661d0f60eec3d4401
Jim (Compositr) <jimke2000@gmail.com>
2022-05-09 17:15:42 +1000

    feat(utils): more emojis
    Signed-off-by: Jim (Compositr) <jimke2000@gmail.com>

--- src/utils/constants/Emojis.ts
+++ src/utils/constants/Emojis.ts
@@ -9,6 +9,10 @@ enum Emojis {
   EXTERNAL = "<:external:971335975928221706>",
   REPLY = "<:reply:972087296633495622>",
   REPLYCONT = "<:replyCont:972087296599945226>",
+  ADD = "<:add:972098874091139102>",
+  ADDUSER = "<:adduser:972098874309238914>",
+  HELP = "<:help:972098873759760395>",
+  GLOBE = "<:globe:972099981106372628>",
 }
 
 export default Emojis;
