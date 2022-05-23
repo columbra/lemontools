1d9c2d1d58b9b92b9d5f1077da24a28abffad76f
Jim (Compositr) <jimke2000@gmail.com>
2022-05-23 16:51:00 +1000

    feat(utils): number utils
    Signed-off-by: Jim (Compositr) <jimke2000@gmail.com>

---
+++ src/utils/number/NumberUtils.ts
@@ -0,0 +1,11 @@
+export default class NumberUtils {
+  /**
+   * Generate a random number between min and max
+   * @param min Minimum number
+   * @param max Maximum number
+   * @returns Random number between min and max
+   */
+  static random(min: number, max: number) {
+    return Math.floor(Math.random() * (max + 1 - min)) + min;
+  }
+}
