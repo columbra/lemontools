9bfb55a495502beef9bdc8dfcf1c27fcf8bdc9b9
Jim (Compositr) <jimke2000@gmail.com>
2022-06-01 12:27:00 +1000

    feat(commands): add more detail to /server
    Signed-off-by: Jim (Compositr) <jimke2000@gmail.com>

--- src/commands/discord/server.ts
+++ src/commands/discord/server.ts
@@ -3,12 +3,30 @@
  * @since v3.0.0
  */
 
+import type { Guild } from "discord.js";
 import Command from "../../classes/commands/Command";
 import Reply from "../../classes/commands/CommandCustomReply";
 import LemonToolsEmbed from "../../classes/embeds/LemonToolsEmbed";
 import { ErrorCodes } from "../../classes/errors/ErrorCode";
 import InteractionUtils from "../../utils/interaction/InteractionUtils";
 import StringUtils from "../../utils/string/StringUtils";
+import ms from "ms"
+
+const explicitDict: { [key in Guild["explicitContentFilter"]]: string } = {
+  ALL_MEMBERS: "Checking messages sent by all members",
+  DISABLED: "Disabled",
+  MEMBERS_WITHOUT_ROLES: "Checking messages from members without roles",
+};
+
+const defaultNotifsDict: { [key in Guild["defaultMessageNotifications"]]: string } = {
+  ALL_MESSAGES: "All messages",
+  ONLY_MENTIONS: "Only mentions"
+};
+
+const mfaDict: { [key in Guild["mfaLevel"]]: string } = {
+  ELEVATED: "MFA required for moderation",
+  NONE: "Disabled"
+};
 
 export default new Command(
   {
@@ -46,13 +64,17 @@ export default new Command(
                 "Verification Level",
                 StringUtils.capitialise(guild.verificationLevel.toLowerCase())
               ),
-              f("Explicit Content Filter", guild.explicitContentFilter),
-              f("AFK Timeout", guild.afkTimeout),
+              f(
+                "Explicit Content Filter",
+                explicitDict[guild.explicitContentFilter]
+              ),
+              // seconds to ms
+              f("AFK Timeout", ms((guild.afkTimeout * 1000))),
               f(
                 "Default Message Notifications",
-                guild.defaultMessageNotifications
+                defaultNotifsDict[guild.defaultMessageNotifications]
               ),
-              f("MFA Level", guild.mfaLevel),
+              f("Multi-Factor Authentication (MFA)", mfaDict[guild.mfaLevel]),
             ],
           },
           ctx.interaction.user
