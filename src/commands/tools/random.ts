bcee4fe2689ed29010dbd141df3adec17e65201b
Jim (Compositr) <jimke2000@gmail.com>
2022-06-03 16:11:35 +1000

    feat(commands): random button
    Signed-off-by: Jim (Compositr) <jimke2000@gmail.com>

--- src/commands/tools/random.ts
+++ src/commands/tools/random.ts
@@ -1,6 +1,11 @@
+import { Emoji, Message, MessageActionRow, MessageButton } from "discord.js";
 import Command from "../../classes/commands/Command";
 import Reply from "../../classes/commands/CommandCustomReply";
 import LemonToolsEmbed from "../../classes/embeds/LemonToolsEmbed";
+import { ErrorCodes } from "../../classes/errors/ErrorCode";
+import config from "../../config";
+import Emojis from "../../utils/constants/Emojis";
+import InteractionUtils from "../../utils/interaction/InteractionUtils";
 import NumberUtils from "../../utils/number/NumberUtils";
 
 export default new Command(
@@ -27,12 +32,60 @@ export default new Command(
     const min = ctx.interaction.options.getNumber("min", true);
     const max = ctx.interaction.options.getNumber("max", true);
     const number = NumberUtils.random(min, max);
-    return ctx.reply(
-      new Reply(
-        new LemonToolsEmbed({
-          description: `**Number:** \`${number}\`\nMin: \`${min}\`\nMax: \`${max}\``,
-        }, ctx.interaction.user)
-      )
+    const reply = await ctx.reply(
+      new Reply({
+        embeds: [
+          new LemonToolsEmbed(
+            {
+              description: `**Number:** \`${number}\`\nMin: \`${min}\`\nMax: \`${max}\``,
+            },
+            ctx.interaction.user
+          ),
+        ],
+        components: [
+          new MessageActionRow().addComponents([
+            new MessageButton()
+              .setCustomId("reroll")
+              .setStyle("PRIMARY")
+              .setEmoji(Emojis.REFRESH)
+              .setLabel(`Reroll`),
+          ]),
+        ],
+      })
     );
+    if (!(reply instanceof Message))
+      return ctx.followUp(
+        new Reply(
+          InteractionUtils.standaloneStdError(
+            "Something went wrong whilst fetching the message.",
+            ErrorCodes.MESSAGE_FETCH_RETURNED_APIMESSAGE
+          )
+        )
+      );
+    const coll = reply.createMessageComponentCollector({
+      componentType: "BUTTON",
+      time: config.bot.collectors.longTimeout,
+      filter: (i) => i.user.id === ctx.interaction.user.id,
+    });
+
+    coll.on("collect", (i) => {
+      const number = NumberUtils.random(min, max);
+      i.update({
+        embeds: [
+          new LemonToolsEmbed(
+            {
+              description: `**New Number:** \`${number}\`\nMin: \`${min}\`\nMax: \`${max}\``,
+            },
+            i.user
+          ),
+        ],
+      });
+    });
+
+    coll.on("end", () => {
+      reply.edit({
+        components: InteractionUtils.disableComponents(reply.components),
+      });
+    });
   }
 );
