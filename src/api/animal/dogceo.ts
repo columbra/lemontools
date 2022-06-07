7fcfc6264a120a8e7980ae964f5241473c315243
Jim (Compositr) <jimke2000@gmail.com>
2022-06-07 21:24:08 +1000

    feat(api): api wrapper for dogceo
    Signed-off-by: Jim (Compositr) <jimke2000@gmail.com>

---
+++ src/api/animal/dogceo.ts
@@ -0,0 +1,25 @@
+/**
+ * @fileoverview API Wrapper for Dog CEO API, gets random dogs
+ * @since v3.0.0
+ */
+
+import axios from "axios";
+import type { MessageEmbedOptions } from "discord.js";
+import type APIWrapper from "../../utils/typings/api/APIWrapper";
+
+export default class DogCEO implements APIWrapper {
+  name = "dogceo";
+  static async get() {
+    const { data } = await axios.get(`https://dog.ceo/api/breeds/image/random`);
+    if (!data.success) throw new Error(`DogCEO API Error: ${data.message}`);
+
+    const embed: MessageEmbedOptions = {
+      image: {
+        url: data.message,
+      },
+      title: `Random Dog!`
+    };
+
+    return embed;
+  }
+}
